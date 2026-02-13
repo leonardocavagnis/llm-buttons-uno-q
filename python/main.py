6764cfc50a9936fbce2f98157226983feaedd43e
Leonardo Cavagnis <l.cavagnis@arduino.cc>
2026-02-13 11:28:21 +0000

    add LLM Brick

--- python/main.py
+++ python/main.py
@@ -1,7 +1,26 @@
-from arduino.app_utils import *
+from arduino.app_bricks.cloud_llm import CloudLLM, CloudModel
+from arduino.app_utils import App, Bridge
 
-def prompt_call_from_button(button: int) -> None:
-    print(f"Button {button} pressed!")
+llm = CloudLLM(
+    model=CloudModel.GOOGLE_GEMINI
+)
+
+def prompt_call_from_button(button):
+    print("Button", button, "pressed!")
+
+    if button == 1:
+        prompt = "Write the recipe for carbonara"
+    elif button == 2:
+        prompt = "Write an Arduino program to blink an LED"
+    elif button == 3:
+        prompt = "Weekend itinerary for Copenhagen"
+    else:
+        print("No prompt for this button")
+        return
+
+    print(prompt)
+    response = llm.chat(prompt)
+    print(response)
 
 Bridge.provide("prompt_call_from_button", prompt_call_from_button)
 
